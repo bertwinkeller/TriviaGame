5351b7830c357029e78933849cde80b72e90734c
Bert Keller <bertwinkeller@gmail.com>
2019-09-16 22:38:10 -0700

    added trivia questions objects

---
+++ assets/javascript/app.js
@@ -0,0 +1,44 @@
+const triviaArray = [
+    {
+        question: 'Which of the following inventions was the first to be patented?',
+        answers: ['chewing gum','rubber band','dishwasher','bicycle'];
+        correctAnswer: 'rubber band'
+    },
+    {
+        question: 'Which of the following empires had no written language?',
+        answers: ['roman empire','incan empire','aztec empire','tang dynasty'],
+        correctAnswer: 'incan empire'
+    }, 
+    {
+        question: 'Which American general famously promised "I shall return"',
+        answers: ['george washington','geroge patton','douglas macarthur','john pershing'],
+        correctAnswer: 'douglas macarthur'
+    },
+    {
+        question: 'When the first Burger King Restaurant opened in 1954, how much did a hamburger cost',
+        answers:['37 cents', '18 cents', '1 dollar', '10 cents' ],
+        correctAnswer: '18 cents'
+    },
+    {
+        question: 'In what ocean was the Battle of Midway Fought?',
+        answers: ['pacific ocean','atlantic ocean','indian ocean','southern ocean'],
+        correctAnswer: 'pacific ocean'
+
+    },
+    {
+        question: 'Who did Spartacus fight in the Third Servile War?',
+        answers: ['romans','phoenicians','carthaginians','persians'],
+        correctAnswer: 'romans'
+
+    },
+    {
+        question: 'Which Soviet satellite was the first to be launched into space in 1957?',
+        answers: ['venera','sputnik','mir','vostok'],
+        correctAnswer: 'sputnik'
+    },
+    {
+        question: 'What was the name of the military nobility of medieval and early-modern Japan?',
+        answers:['kazoku','ninja','emperor','samurai'],
+        correctAnswer: 'samurai'
+    }
+]
